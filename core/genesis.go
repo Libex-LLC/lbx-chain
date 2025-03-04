bnb-chain/bsc

params: add LorentzTime

core/genesis.go
@@ -244,6 +244,7 @@ type ChainOverrides struct {
 	OverridePassedForkTime *uint64
 	OverridePascal         *uint64
 	OverridePrague         *uint64
+	OverrideLorentz        *uint64
 	OverrideVerkle         *uint64
 }
 
@@ -285,6 +286,9 @@ func SetupGenesisBlockWithOverride(db ethdb.Database, triedb *triedb.Database, g
 			}
 			if overrides != nil && overrides.OverridePrague != nil {
 				config.PragueTime = overrides.OverridePrague
+			}
+			if overrides != nil && overrides.OverrideLorentz != nil {
+				config.LorentzTime = overrides.OverrideLorentz
 			}
 			if overrides != nil && overrides.OverrideVerkle != nil {
 				config.VerkleTime = overrides.OverrideVerkle

Libex-LLC/lbx-chain

core/genesis.go
@@ -266,6 +266,7 @@ type ChainOverrides struct {
 	OverridePassedForkTime *uint64
 	OverridePascal         *uint64
 	OverridePrague         *uint64
+	OverrideLorentz        *uint64
 	OverrideVerkle         *uint64
 }
 
@@ -289,6 +290,9 @@ func (o *ChainOverrides) apply(cfg *params.ChainConfig) error {
 	}
 	if o.OverridePrague != nil {
 		cfg.PragueTime = o.OverridePrague
+	}
+	if o.OverrideLorentz != nil {
+		cfg.LorentzTime = o.OverrideLorentz
 	}
 	if o.OverrideVerkle != nil {
 		cfg.VerkleTime = o.OverrideVerkle